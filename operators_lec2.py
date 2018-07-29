850677a2b273df174c0c8579bfe7350bc534b956
pythonjitendra <41856742+pythonjitendra@users.noreply.github.com>
2018-07-29 22:43:31 +0530

    Operator lectures

---
+++ operators_lec2.py
@@ -0,0 +1,120 @@
+import operator
+
+# arithematic operators
+#+ ,-,* / ,//,%
+
+'''print(2+2 +45*34)
+a,b,c=10,20,34
+print(operator.add(a,b))'''
+
+#print(operator.sub(20,10))
+#print(operator.truediv(20,10))
+#print(operator.pow(10,20))
+
+# comparison operators
+
+#==,!=,>=<=,>,<  <>
+'''age=23
+nage=23
+if age < 24:
+    print("Age Matched")
+else:
+    print("Not Matched")
+
+
+print(operator.eq(age,12))
+print(operator.ge(age,nage))
+'''
+#Assignment Operators
+
+'''for(i=0i< 5;i++)
+    {
+        
+    }
+n=4
+if n<5:
+    n=n/2
+    print(n)
+'''
+
+# floordivion
+
+'''print(5/2)
+print(5//2)'''
+
+#Logical Operators
+
+#print(5 ^10)
+#print(operator.xor(5,10))
+#print(int('10100',2))
+
+'''print(4<<3)
+age=15
+age1=20
+if(age==15 or age1==20):
+    print('matched')
+else:
+    print('not matched')
+'''
+
+
+'''x1=30
+y1=20
+
+if(x1 and y1):
+    x1=40
+    if(x1 and y1):
+        print(x1,y1)
+    else:
+        print("not matched")
+
+else:
+    print('NM')
+'''
+'''
+x = True
+y =''
+
+
+if(x and y):
+    print("true")
+else:
+    print("false")
+'''
+
+# Memebership
+
+
+'''x=[20,30,40,50]
+
+if (21 in x):
+    print("True")
+else:
+    print("False")'''
+
+
+#is is not
+'''
+s=20
+print(s is not 23)
+'''
+
+#ternary
+
+
+#[true][condition] else "conditions"
+
+a=10
+b=20
+
+print(a if a > b else b)
+
+print("a is greter than b" if a==b else "b is greater than a"    if a>b else "b is greter than a")
+
+#tuple
+
+#(values)[conditin]
+
+print({False:b,True:a}[ a > b])
+
+
