84911079bcfcfec5c42ccf6ba22f9b19533ecdc3
pythonjitendra <41856742+pythonjitendra@users.noreply.github.com>
2018-07-29 23:29:18 +0530

    Numbers Lectures

---
+++ Numbers_lec.py
@@ -0,0 +1,83 @@
+# Int and Long
+
+'''d=1000000000000000000000000000000000000000000000000000000000000000000000
+print(d * 2)
+#print(d))'''
+
+
+# Float
+'''ff='12122.343344'
+print(float(ff))
+
+f=12222
+print(float(f))'''
+
+
+#complex
+import math
+
+'''a,b=10,20
+z=complex(a,b)
+print(z.real)
+print(z.imag)'''
+
+# Mathematical Functions
+import math
+'''print(abs(11.22))
+
+print(math.ceil(2.34))
+
+print(math.fabs(10))
+
+print(math.fabs(-10.23))
+
+#print(help(math))'''
+
+'''print(min(12,34,345,56,56,7,7,8,8,99,6,6,6775,656))
+print(max(12,34,345,56,56,7,7,8,8,99,6,6,6775,656))
+print(math.pow(4,3))'''
+#print(round(12.5))
+#print(math.sqrt(16))
+
+
+import random
+
+# choice
+
+
+#print(random.choice(seq))
+
+'''print(random.randrange(2,10,3))
+
+print(random.random())'''
+
+'''random.seed(10)
+print(random.random())'''
+
+'''
+seq11=[12,343,4454,6,67,67,8,87,89,9,10,90]
+random.shuffle(seq11)
+print(seq11)
+
+s=random.uniform(2,4)
+print(s)
+
+#trigonometric functions
+import math
+print(math.acos(-1))
+print(math.acos(1))
+print(math.acos(0))
+
+print(math.asin(-1))
+print(math.asin(1))
+print(math.asin(0))
+
+
+print(math.atan2(2,3))
+
+print(math.degrees(1))
+print(math.radians(57.2958))
+'''
+
+
+
