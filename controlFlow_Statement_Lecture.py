85dc6bd350f7f0b227656ad01dca1ad3817cacf2
Jitendra <41856742+pythonjitendra@users.noreply.github.com>
2019-01-13 09:05:54 +0530

    Added Control flow statement

---
+++ controlFlow_Statement_Lecture.py
@@ -0,0 +1,104 @@
+## Loop and Control Statement
+# if statement
+# else statement
+# elif statement
+# Loop Control Statements
+# While loop
+# For loop
+# Loop Control Statements
+# Break Statement
+# Continue Statement
+# Pass Statement
+
+
+# if ,elif,else
+# for ,while
+
+
+# If Statement
+
+#Syntax
+'''if(condition): # Will matched
+    "True"
+elif:
+    "False"'''
+
+#
+'''out=int(input("Enter the any number : "))
+if(out > 0):
+    print(out, "is positive number")
+elif(out<0):
+    print(out, "is Negitive number")
+else:
+    print("zero number")'''
+
+
+## Example
+'''age=50
+if(age >=50):
+    print(age, "is correct")
+else:
+    print(age, "is incorrect")'''
+
+
+# while
+
+'''i=0
+while i < 10:
+   print(i)
+   i+=1'''
+
+
+# For
+
+'''lst=["name","age"]
+for item in lst:
+    print(item,len(item))'''
+
+
+ #Loop Control Statements
+# Break Statement
+# Continue Statement
+# Pass Statement
+
+'''for i in range(10):
+    pass
+
+class Birds:
+    pass
+
+
+def func():
+    pass'''
+
+
+# Break Statement
+'''x=2
+for n in range(2, 10):
+    if n % x == 0:
+        print(n, 'is even number')
+        break
+    else:
+        print(n, 'is a odd number')'''
+
+# Continue Statement
+'''x=2
+for n in range(2, 10):
+    if n % x == 0:
+        print(n, 'is even number')
+        continue
+    else:
+        print(n, 'is a odd number')'''
+
+
+
+
+
+
+
+
+
+
+
+
+
